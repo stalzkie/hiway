6b72fa69500cedf0e50142f1dee7200feb84219b
stalzkie <dstalingrad@gmail.com>
2025-09-03 16:25:22 +0800

    placed test to frontend

--- apps/frontend/test/widget_test.dart
+++ apps/frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hiway_app/main.dart';
+import 'package:hiway_app/apps/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
